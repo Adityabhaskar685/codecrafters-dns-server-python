63dac6905920c6278d70918c9db637d3c7ffbe8e
Aditya Bhaskar <adityaverma685@gmail.com>
2024-01-27 22:48:01 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,79 @@
 import socket
+from dataclasses import dataclass
+import struct
+from typing import ClassVar
+
+@dataclass
+class DNSMessage:
+    id : int
+    qr : int
+    op_code: int
+    aa : int
+    tc : int
+    rd : int
+    ra: int
+    z : int
+    rcode : int
+    qdcount: int
+    ancount: int
+    nscount: int
+    arcount: int
+
+    ID_LEN: ClassVar[int] = 16
+    QR_LEN: ClassVar[int] = 1
+    OP_CODE_LEN: ClassVar[int] = 4
+    AA_LEN : ClassVar[int] = 1
+    TC_LEN : ClassVar[int] = 1
+    RD_LEN : ClassVar[int] = 1
+    RA_LEN : ClassVar[int] = 1
+    Z_LEN : ClassVar[int] = 3
+    RCODE_LEN: ClassVar[int] = 4
+    QDCOUNT_LEN : ClassVar[int] = 16
+    ANCOUNT_LEN : ClassVar[int] = 16
+    NSCOUNT_LEN : ClassVar[int] = 16
+    ARCOUNT_LEN : ClassVar[int] = 16
+
+
+    def serialize(self) -> str :
+        # assumes bit endian
+        combined1 = 0
+        cur_shift = 0
+        for val, delta_shift in zip(
+            (self.rd,self.tc, self.aa, self.op_code, self.qr),
+            (
+                DNSMessage.RD_LEN,
+                DNSMessage.TC_LEN,
+                DNSMessage.AA_LEN,
+                DNSMessage.OP_CODE_LEN,
+                0
+
+            )
+        ):
+            combined1 |= val << cur_shift
+            cur_shift += delta_shift
+
+        # assume big endian
+        combined2 = 0
+        cur_shift = 0
+        for val, delta_shift in zip(
+            (self.rcode, self.z, self.ra),
+            (DNSMessage.RCODE_LEN, DNSMessage.Z_LEN, DNSMessage.RA_LEN)
+        ):
+            combined2 |= val << cur_shift
+            cur_shift += delta_shift
+
+
+        return struct.pack(
+            ">HBBHHHH",
+            self.id,
+            combined1,
+            combined2,
+            self.qdcount,
+            self.ancount,
+            self.nscount,
+            self.arcount
+        )
+    
 
 
 def main():
@@ -9,16 +84,37 @@ def main():
     #
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
+
+    first = True
+
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
     
             response = b""
+            if first:
+                response_data = DNSMessage(
+                    id = 1234,
+                    qr = 1,
+                    op_code = 0,
+                    aa = 0,
+                    tc = 0,
+                    rd = 0,
+                    ra = 0,
+                    z = 0,
+                    rcode = 0,
+                    qdcount= 0,
+                    ancount= 0,
+                    nscount= 0,
+                    arcount= 0
+                )
+            response = response_data.serialize()
     
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
+
 
 
 if __name__ == "__main__":
