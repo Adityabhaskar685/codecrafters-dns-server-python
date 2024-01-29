5cdbb382ddeafd8fe33685b5314c54ccbde22ee8
Aditya Bhaskar <adityaverma685@gmail.com>
2024-01-29 15:47:30 +0530

    6th stage

--- app/main.py
+++ app/main.py
@@ -1,174 +1,217 @@
 import socket
-from dataclasses import dataclass
 import struct
-from typing import ClassVar
+from dataclasses import dataclass
+from typing import List, Tuple
+
+HEADER_STRUCT_FORMAT = '>HBBHHHH'
+QUESTION_STURCT_FORMAT = '>HH'
+ANSWER_STRUCT_FORMAT = '>HHHLH'
+BIG = 'big'
+
+
+def name_to_bytes(name) -> bytes:
+    byte_arr = bytearray()
+    for part in name.split('.'):
+        byte_arr.extend(len(part).to_bytes(1, BIG))
+        byte_arr.extend(part.encode())
+    byte_arr.extend(0).to_bytes(1, BIG)
+    return byte_arr
+
+def parse_name(buffer, idx) -> Tuple[str, int]:
+    res = []
+    while buffer[idx] != 0:
+        if buffer[idx] >> 6 == 3:
+            offset = int.from_bytes(buffer[idx:idx+2], BIG) & ((1 << 6))
+            compresed_name, _ = parse_name(buffer, offset)
+            res.extend(compresed_name.split("."))
+            idx += 1
+            break
+        length = buffer[idx]
+        res.append(buffer[idx+1: idx: +1+length].decode())
+        idx += 1 + length
+    return ".".join(res), idx + 1
 
 @dataclass
-class DNSMessage:
-    id : int
-    qr : int
-    op_code: int
-    aa : int
-    tc : int
-    rd : int
+class Header:
+    id: int
+    qr: int
+    opcode: int
+    aa: int
+    tc: int
+    rd: int
     ra: int
-    z : int
-    rcode : int
+    rcode: int
     qdcount: int
     ancount: int
     nscount: int
     arcount: int
 
-    PACK_STR: ClassVar[str] = ">HBBHHHH"
-
-    ID_LEN: ClassVar[int] = 16
-    QR_LEN: ClassVar[int] = 1
-    OP_CODE_LEN: ClassVar[int] = 4
-    AA_LEN : ClassVar[int] = 1
-    TC_LEN : ClassVar[int] = 1
-    RD_LEN : ClassVar[int] = 1
-    RA_LEN : ClassVar[int] = 1
-    Z_LEN : ClassVar[int] = 3
-    RCODE_LEN: ClassVar[int] = 4
-    QDCOUNT_LEN : ClassVar[int] = 16
-    ANCOUNT_LEN : ClassVar[int] = 16
-    NSCOUNT_LEN : ClassVar[int] = 16
-    ARCOUNT_LEN : ClassVar[int] = 16
-
-
-    @staticmethod
-    def from_header_bytes(buf):
-        (
-            id,
-            combined1,
-            combined2,
-            qdcount,
-            ancount,
-            nscount,
-            arcount
-        ) = struct.unpack(DNSMessage.PACK_STR, buf[:12])
-
-        op_code_shift = DNSMessage.RD_LEN + DNSMessage.TC_LEN + DNSMessage.AA_LEN
-        op_code = (combined1 >> op_code_shift) & 0xF
-        r_code = 0 if op_code == 0 else 4
-        rd_shift = 0
-        rd = (combined1 >> rd_shift) & 0x1
-
-        return DNSMessage(
-            id = id,
-            qr = 1,
-            op_code = op_code,
-            aa = 0,
-            tc = 0,
-            rd = rd,
-            ra = 0,
-            z = 0,
-            rcode = r_code,
-            qdcount= 1,
-            ancount= 1,
-            nscount= 0,
-            arcount= 0
-        )
-
-    
-    def serialize(self) -> str :
-        # assumes bit endian
-        combined1 = 0
-        cur_shift = 0
-        for val, delta_shift in zip(
-            (self.rd,self.tc, self.aa, self.op_code, self.qr),
-            (
-                DNSMessage.RD_LEN,
-                DNSMessage.TC_LEN,
-                DNSMessage.AA_LEN,
-                DNSMessage.OP_CODE_LEN,
-                0
-
-            )
-        ):
-            combined1 |= val << cur_shift
-            cur_shift += delta_shift
-
-        # assume big endian
-        combined2 = 0
-        cur_shift = 0
-        for val, delta_shift in zip(
-            (self.rcode, self.z, self.ra),
-            (DNSMessage.RCODE_LEN, DNSMessage.Z_LEN, DNSMessage.RA_LEN)
-        ):
-            combined2 |= val << cur_shift
-            cur_shift += delta_shift
-
-
+    @property
+    def as_bytes(self) -> bytes:
+        flag_a = self.qr << 7 | self.opcode << 3 | self.aa << 2 | self.tc << 1 | self.rd
+        flag_b = self.ra << 7 | self.z << 4 | self.rcode
         return struct.pack(
-            DNSMessage.PACK_STR,
+            HEADER_STRUCT_FORMAT,
             self.id,
-            combined1,
-            combined2,
+            flag_a,
+            flag_b,
             self.qdcount,
             self.ancount,
             self.nscount,
             self.arcount
+        )   
+    
+    @staticmethod
+    def parse_buffer(buffer):
+        (id, flag_a, flag_b, qdcount, ancount, nscount, arcount) = struct.unpack(
+            HEADER_STRUCT_FORMAT,
+            buffer[: struct.clacsize(HEADER_STRUCT_FORMAT)]
         )
 
-def encode_url(url):
-    response = b''
-    domains = url.split('.')
-    for domain in domains:
-        response += struct.pack('>B', len(domain))
-        response += b''.join((struct.pack('>B', ord(char)) for char in domain))
-
-    response += struct.pack('>B', 0)
-    return response 
+        qr = (flag_a >> 7) & 1
+        opcode = (flag_a >> 3) & ((1 << 4) - 1)
+        aa = (flag_a >> 2) & 1
+        tc = (flag_a >> 1) & 1
+        rd = flag_a & 1
+        ra = (flag_b >> 7) & 1
+        z = (flag_b >> 4) & ((1 << 3) - 1)
+        rcode = flag_b & ((1 << 4) - 1)
+        return Header(
+            id = id,
+            qr = qr,
+            opcode = opcode,
+            aa = aa,
+            tc = tc,
+            rd = rd,
+            ra = ra,
+            z = z,
+            rcode = rcode,
+            qdcount = qdcount,
+            ancount = ancount,
+            nscount = nscount,
+            arcount = arcount
+        )
     
 
+@dataclass
+class Question:
+    name: str
+    typ: int
+    cls: int
+
+    @property
+    def as_bytes(self) -> bytes:
+        return name_to_bytes(self.name) + struct.pack(QUESTION_STURCT_FORMAT, self.type, self.cls)
+    
+
+    @staticmethod
+    def parse_buffer(buffer, idx):
+        name, idx = parse_name(buffer, idx)
+        typ, cls = struct.unpack(QUESTION_STURCT_FORMAT, buffer[idx: idx + struct.calcsize(QUESTION_STURCT_FORMAT)])
+        return Question(name, typ, cls), idx + struct.calcsize(QUESTION_STURCT_FORMAT)
+    
+
+@dataclass
+class Answer:
+    name: str
+    typ: int
+    cls : int
+    ttl : int
+    length: int
+    data: bytes
+
+    @property
+    def as_bytes(self) -> bytes:
+        return name_to_bytes(self.name) + struct.pack(ANSWER_STRUCT_FORMAT, self.typ, self.cls, self.ttl, self.length) + self.data
+    
+    
+    @staticmethod
+    def parse_buffer(buffer, offset):
+        name, idx = parse_name(buffer, offset)
+        typ, cls, ttl, length = struct.unpack(ANSWER_STRUCT_FORMAT, buffer[idx: idx + struct.calcsize(ANSWER_STRUCT_FORMAT)])
+        data = buffer[idx + struct.calcsize(ANSWER_STRUCT_FORMAT): ]
+        return Answer(name = name, typ = typ, cls = cls , ttl = ttl, rdlength  = length, data = data)
+    
+
+@dataclass
+class DNSMessage:
+    header = Header
+    questions = List[Question]
+    answers = List[Answer]
+
+    @property
+    def as_bytes(self) -> bytes:
+        resp = bytearray()
+        resp.extend(self.header.as_bytes)
+        for q in self.questions:
+            resp.extend(q.as_bytes)
+
+        for a in self.answers:
+            resp.extend(a.as_bytes)
+        return resp
+    
+    @staticmethod
+    def parse_buffer(buffer):
+        header = Header.parse_buffer(buffer)
+        questions = []
+        offset = struct.calcsize(HEADER_STRUCT_FORMAT)
+        while offset < len(buffer):
+            question, offset = Question.parse_buffer(buffer, offset)
+            questions.append(question)       
+        return DNSMessage(header = header, questions = questions, answers = [])      
+
+
+def create_header(msg : Header) -> bytes:
+    flag_a = msg.qr << 7 | msg.opcode << 3 | msg.aa << 2 | msg.tc << 1 | msg.rd
+    flag_b = msg.ra << 7 | msg.z << 4 | msg.rcode
+    return struct.pack(
+        HEADER_STRUCT_FORMAT,
+        msg.id,
+        flag_a,
+        flag_b,
+        msg.qdcount,
+        msg.ancount,
+        msg.nscount,
+        msg.arcount
+    )  
 
 def main():
-    RDLENGTH = 4
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(("127.0.0.1", 2053))
-
-    type_dict_python_int = {'A' : 1}
-    type_dict = {k : struct.pack('>H', v) for k, v in type_dict_python_int.items()}
-
-    class_dict_python_int = {"IN" : 1}
-    class_dict = {k : struct.pack('>H', v) for k, v in class_dict_python_int.items()}
-
-    first = True
-
+    udp_socket.bind(('127.0.0.1', 2053))
     while True:
-        try:
+        try: 
             buf, source = udp_socket.recvfrom(512)
-            response_header_data = DNSMessage.from_header_bytes(buf)
-            response = response_header_data.serialize()
-
-            # added the question section 
-            idx = 12
-            while buf[idx] != 0 and idx < len(buf):
-                idx += 1
-
-            domain_data = buf[12:idx] + struct.pack('>B', 0x00)
+            rec_msg = DNSMessage.parse_buffer(buf)
+            
+            questions = []
+            answers = []
+            for q in rec_msg.questions:
+                questions.append(q)
+                answers.append(Answer(name = q.name, typ = 1, cls = 1, ttl = 60, length = 4, data = b'\x08\x08\x08\x08'))
 
 
+            header = Header(
+                id = rec_msg.header.id,
+                qr = 1,
+                opcode= rec_msg.header.opcode,
+                aa = 0
+                tc = 0,
+                rd = rec_msg.header.rd,
+                ra = 0,
+                z = 0,
+                rcount = 0 if rec_msg.header.qdcount == 0 else 4,
+                qdcount= len(questions),
+                ancount= len(answers),
+                nscount =  0,
+                arcount = 0
+            )
 
-            question = domain_data + type_dict['A'] + class_dict['IN']
-            response += question
 
-            answer = domain_data + type_dict['A'] + class_dict['IN'] + struct.pack('>I', 60) + struct.pack('>H', RDLENGTH) + struct.pack('>BBBB', 8,8,8,8)
-            response += answer
-
-    
-            udp_socket.sendto(response, source)
+            resp_msg = DNSMessage(header = header, questions = questions, answers = answers)
+            udp_socket.sendto(resp_msg.as_bytes, source)
+            
         except Exception as e:
-            print(f"Error receiving data: {e}")
+            print(e)
             break
 
-
-
-if __name__ == "__main__":
-    main()
+                   
